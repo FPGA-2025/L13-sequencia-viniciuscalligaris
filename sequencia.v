0ee7c7f875081d2a0e6333c33f71e7ac415d9ca8
Vinícius César Bertolo Calligaris <vinicius.cesar7@hotmail.com>
2025-05-01 11:39:18 -0300

    Finalizado

--- sequencia.v
+++ sequencia.v
@@ -1,15 +1,41 @@
 module Sequencia (
     input wire clk,
     input wire rst_n,
-
     input wire setar_palavra,
     input wire [7:0] palavra,
-
     input wire start,
     input wire bit_in,
-
     output reg encontrado
 );
-// Seu codigo aqui    
+    reg [7:0] palavra_buscada;          
+    reg [7:0] deslocamento_recebido;             
+    reg buscando;                   
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            palavra_buscada <= 8'b0;
+            deslocamento_recebido <= 8'b0;
+            encontrado <= 1'b0;
+            buscando <= 1'b0;
+        end else begin
+            if (setar_palavra) begin
+                palavra_buscada <= palavra;
+                encontrado <= 1'b0;
+                buscando <= 1'b0;
+            end else if (start) begin
+                buscando <= 1'b1;
+                encontrado <= 1'b0;
+            end
+
+            if (buscando && !encontrado) begin
+                deslocamento_recebido <= {deslocamento_recebido[6:0], bit_in};
+
+                if (deslocamento_recebido == palavra_buscada) begin
+                    encontrado <= 1'b1;
+                    buscando <= 1'b0; 
+                end
+            end
+        end
+    end
 
 endmodule
